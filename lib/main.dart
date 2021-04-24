d58fff7bb61c6bf8f2c969c5cb57028d5e5ffc84
Gilmar Sanchez Cuartas <gsanchez@tsgroup.local>
2021-04-24 11:21:22 -0500

    Personalizar tema de la app

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,12 @@ class MyApp extends StatelessWidget {
         'home' : (_) => HomePage(), 
         'mapa' : (_) => MapaPage(),
       },
+      theme: ThemeData(
+        primaryColor: Colors.teal,
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.teal
+        )
+      ),
     );
   }
 }
